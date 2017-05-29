3bd71fd87db36043146b641b1182a57f142418e0
Yunsik Park <space214@gmail.com>
2017-05-30 01:02:21 +0900

    applied djangogirls CSS

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,6 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    # posts = Post.objects.filter(
+    #     published_date__lte = timezone.now()
+    # )
+    posts = Post.objects.all()
+    context = {
+        'title' : 'PostList from post_list view',
+        'posts': posts,
+    }
+    return render(request, 'blog/post_list.html', context=context)
 
